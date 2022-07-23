db570925947861edcf0dc02d0a354dadceb61d6d
lesnuages <1799430+lesnuages@users.noreply.github.com>
2022-07-23 10:02:22 -0700

    Avoid panic

--- cmd/root.go
+++ cmd/root.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"path"
 
+	"github.com/lesnuages/thm/pkg/utils"
 	"github.com/lesnuages/thm/pkg/vmware"
 	"github.com/spf13/cobra"
 )
@@ -20,9 +21,6 @@ var rootCmd = &cobra.Command{
 	Use:   "thm",
 	Short: "The Manager",
 	Long:  `A simple CLI tool to manage a VSphere cluster.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -36,28 +34,24 @@ func Execute() {
 
 func init() {
 	var err error
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
+
 	configPath := path.Join(os.Getenv("HOME"), ".config", "thm")
 	config, err = vmware.LoadConfig(configPath)
 	if err != nil {
 		err = os.MkdirAll(configPath, 0755)
 		if err != nil {
-			panic(err)
+			utils.PrintError("%v", err)
+			return
 		}
 		err = ioutil.WriteFile(path.Join(configPath, "config.env"), []byte(vmware.GetDefaultConfig()), 0644)
 		if err != nil {
-			panic(err)
+			utils.PrintError("%v", err)
+			return
 		}
 		config, err = vmware.LoadConfig(configPath)
 		if err != nil || config == nil {
-			panic(err)
+			utils.PrintError("%v", err)
+			return
 		}
 	}
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.thm.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
